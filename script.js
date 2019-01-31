5765e571de1bd532cbe2baac540e5b8227dbfaaa
Samantha Cayla Bajis <Sammi.Bajis@gmail.com>
2019-01-30 19:34:41 -0500

    gyfiuyfg

--- script.js
+++ script.js
@@ -1,86 +1,66 @@
-"use strict";
-
-class friendInfo {                                     
-    constructor(name, friendly, flyingSkill, dejarikSkill, fearGE) {     
-        this.name = name;
-        this.friendly = friendly;                          
-        this.flyingSkill = flyingSkill;                     
-        this.dejarikSkill = dejarikSkill;
-        this.fearGE = fearGE;                     
+class friendInfo {
+    constructor(name, flyingSkill, dejarikSkill, galacticFear, currentCandidate) {
+      this.name = name;
+      this.flyingSkill = parseInt(flyingSkill);
+      this.dejarikSkill = parseInt(dejarikSkill);
+      this.galacticFear = parseInt(galacticFear);
+      this.currentCandidate = currentCandidate;
     }
-}
+  }
 
-let currentCandidate = 0;
-let highestScore = 0;
-
-
-class Friend {                        
+  class Friend {
     constructor() {
-        this.newfriend = [];           
+      this.newfriend = [];
     }
     add(info) {
-        this.newfriend.push(info);          
-    }
-    deleteAt(index) {
-        this.newfriend.splice(index, 1);     
-    }
-    Score(){
-        let i;
-            for (i = 0; i < this.newfriend.length; i++) {
-                currentCandidate = this.newfriend[i].friendly + this.newfriend[i].flyingSkill + this.newfriend[i].dejarikSkill - this.newfriend[i].fearGE;
-                
-            if (currentCandidate > highestScore) {
-                highestScore = currentCandidate;
-            }
-            
-
-        }
-        console.log(this.newfriend[i].name);
+      this.newfriend.push(info);
     }
     print() {
-        for (let i = 0; i < this.newfriend.length; i++) {      
-            console.log(this.newfriend.highestScore[i]);                
-        }
-
+      for (let i = 0; i < this.newfriend.length; i++) {
+        console.log(this.newfriend[i]);
+      }
+    } 
+    rank() {
+      let bestFriend = null;
+      let worstFriend = null;
+      for (let i = 0; i < this.newfriend.length; i++) {
+            const friends = this.newfriend[i];
+            friends.currentCandidate = friends.flyingSkill + friends.dejarikSkill - friends.galacticFear;
+            if (friends.currentCandidate > bestFriend){
+              bestFriend = friends.name;
+            }
+              console.log(`${bestFriend} is the best friend for Chewbacca.`);
+            if (friends.currentCandidate > worstFriend){
+              worstFriend = friends.name;
+            }
+            console.log(`${worstFriend} is the worst friend for Chewbacca.`);
+      }
     }
+  }
 
-}
+const user = new Friend;
+user.add(new friendInfo("Spongebob", 5, 5, 2)); // 8
+user.add(new friendInfo("Patrick", 6, 6, 2));   // 10
+user.add(new friendInfo("Squidward", 2, 6, 1)); // 5
 
 
-const user = new Friend()
-console.log(user);
-user.add(new friendInfo("Spongebob", 2, 0, 2, 2)); 
-user.add(new friendInfo("Patrick", 4, 1, 1, 1));
-user.print();
-
-/* function Score(){
-    for (let i = 0; i < this.newfriend.length; i++) {
-        currentCandidate = this.newfriend[i].friendly + this.newfriend[i].flyingSkill + this.newfriend[i].dejarikSkill - this.newfriend[i].fearGE;
-        
-    if (currentCandidate > highestScore) {
-        highestScore = currentCandidate
-    }
-} */
-
-user.Score();
-
-// while loop add remove delete or quit
-while (true) {                 
-    const choice = prompt("Would you like to add, print, delete a new friend or quit?");
-    if (choice === "add") {                                                     
+while (true) {
+    const choice = prompt("Would you like to add a new potential friend, print the list or quit");
+    if (choice === "add") {
         const name = prompt("Enter name: ");
-        const friendly = prompt("Enter friendliness: ");
         const flyingSkill = prompt("Enter flying skill: ");
-        const dejarikSkill = prompt("Enter Dejarik skill : ");
-        const fearGE = prompt("Enter Fear level : ");
-        user.add(new (name,  friendly, flyingSkill, dejarikSkill, fearGE));
-    } else if (choice === "print") {                                          
+        const dejarikSkill = prompt("Enter Dejarik skill: ");
+        const galacticFear = prompt("Enter fear level: ");
+        user.add(new(name, flyingSkill, dejarikSkill, galacticFear));
+    } else if (choice === "print") {
         user.print();
-    } else if (choice === "delete") {                                          // Deleting a contact by using the splice(); method and then deleting which ever index the user chooses through the prompt
-        const del = prompt("What entry do you want to delete?");
-        user.deleteAt(del);
-    } else if (choice === "quit") {                                            // Ending the loop with a break and a console.log
-        console.log("Okay, see you later.");
+        user.rank();
+    } else if (choice === "quit") {                                           
+        console.log("Okay, see you later!!");
         break;
     }
 }
+
+
+
+
